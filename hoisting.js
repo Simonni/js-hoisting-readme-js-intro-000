06fc4b30e4eb7a6233a06628f52f9c65592d195f
Simon Kiflay <szkiflay@gmail.com>
2018-08-11 17:05:31 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,32 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
+callMe();
+console.log(lyric);
 
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  var thisIsCrazy = function crazy(){
+    console.log("hey!!!");
   }
 }
+thisIsCrazy();
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
+console.log(name);
+  sayMyName();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
+sayMy();
